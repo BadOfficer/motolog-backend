5172af7f3b5ee7a670fac4445c549c5a64a74f6b
BadOfficer <tarasbondarenko2005@gmail.com>
2026-05-05 01:35:29 +0300

    Added: - User roles

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -1,13 +1,27 @@
-import { Body, Controller, Delete, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from 'src/generated/prisma/enums';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   async createCategory(@Body() categoryDto: CreateCategoryDto) {
     return this.categoriesService.create(categoryDto);
   }
